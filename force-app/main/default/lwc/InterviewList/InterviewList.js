9ea52de65ffa2115cb9bd4d9528b1ebb49521d4a
jrattanpal <jrattanpal@gmail.com>
2019-03-12 14:01:50 -0400

    Sparkle adk (#91)
    * Get Sparkle repo ready for ADK
    
    * rename folder
    
    * rename folder
    
    * rename folder
    
    * Rename
    
    * Rename
    
    * Rename
    
    * Rename
    
    * Rename
    
    * Rename
    
    * Add data import
    
    * Add data import
    
    * Add data script
    
    * Reset user password on scratch org creation so user gets an email
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Run data setup file in orgInit.sh
    
    * Run data setup file in orgInit.sh

--- force-app/main/default/lwc/InterviewList/InterviewList.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class InterviewList extends LightningElement {}
---
+++ orgInit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#create scratch org
+sfdx force:org:create -f config/project-scratch-def.json -a FSCADK2 --setdefaultusername -d 1
+
+sfdx force:source:push 
+
+sfdx force:user:permset:assign -n Lightning_Components_Sample_Pack_Recruiting
+
+sfdx force:data:tree:import --plan sfdx-out/plan.json
+
+#Run Apex commands as needed
+sfdx force:apex:execute -f config/create-demo-data-setup.apex
+
+sfdx force:org:open
+
